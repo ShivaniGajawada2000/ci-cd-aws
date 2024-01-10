e07253cb164afa42dc5f9389abbe1d8bef062bc6
ShivaniGajawada2000 <111433899+ShivaniGajawada2000@users.noreply.github.com>
2024-01-10 17:53:26 +0530

    Update AwsCiCdApplication.java

--- src/main/java/com/aws/awscicd/AwsCiCdApplication.java
+++ src/main/java/com/aws/awscicd/AwsCiCdApplication.java
@@ -11,20 +11,11 @@ import java.util.List;
 @SpringBootApplication
 @RestController
 public class AwsCiCdApplication {
-	@GetMapping("/getAll")
-	public List<Student> getData(){
-		List<Student> list=new ArrayList<>();
-		Student s1=new Student(1L,"shalem","ECE");
-		Student s2=new Student(2L,"Chintu","CSE");
-		Student s3=new Student(3L,"Philip","EEE");
-		Student s4=new Student(4L,"Fruity","B.Sc");
-		list.add(s1);
-		list.add(s2);
-		list.add(s3);
-		list.add(s4);
-		return list;
-	}
+	@GetMapping("/getMessage")
+	public String getMessage(){
+		return "Hello World";
 
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AwsCiCdApplication.class, args);
 	}
